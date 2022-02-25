6aed24b94494246af2f1a5db2b5029528c6296ec
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-25 11:48:18 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Task3;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new()
+            {
+                { "Нулевой", 0 },
+                { "Первый", 1 },
+                { "Второй", 2 },
+                { "Третий", 3 },
+                { "Четвертый", 4 }
+            };
+
+            foreach (Task3.KeyValuePair<string, int> item in myDictionary)
+            {
+                Console.WriteLine($"Key => {item.Key}, Value => {item.Value}");
+            }
+
+            Console.WriteLine(new String('-', 40));
+            try
+            {
+                Console.WriteLine(myDictionary["Нулевой"]);
+                Console.WriteLine(myDictionary["Десятый"]);
+            }
+            catch (KeyNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine(new String('-', 40));
+            try
+            {
+                myDictionary["Первый"] = 10;
+                myDictionary["Шестой"] = 10;
+            }
+            catch (KeyNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine(new String('-', 40));
+            foreach (Task3.KeyValuePair<string, int> item in myDictionary)
+            {
+                Console.WriteLine($"Key => {item.Key}, Value => {item.Value}");
+            }
+        }
+    }
+}
