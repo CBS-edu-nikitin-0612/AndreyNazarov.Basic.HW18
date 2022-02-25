f30ce2582a3620c7115a320c81fce8286c3db246
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-25 12:10:00 +0300

    feat: Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    // Пример использования загрузил на http://lms.cbs.com.ua/mod/assign/view.php?id=378
+    public class MyClass
+    {
+        public void TestMethod()
+        {
+            Console.WriteLine("Hello worls from " + this);
+        }
+    }
+}
