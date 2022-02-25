ab7dd0f67caada15699750195bd277583720846a
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-02-25 12:10:14 +0300

    feat: Task4

---
+++ Task18/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+        }
+    }
+}
+namespace MyNamespace
+{
+    public class MyClass
+    {
+        // Пример использования загрузил на http://lms.cbs.com.ua/mod/assign/view.php?id=378
+        public void MyMethod()
+        {
+            Console.WriteLine("Hello world from " + this);
+        }
+    }
+}
